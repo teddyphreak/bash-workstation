7ecd8c2e6a5eabd54c16e5ff2d75ac210ad312a8
Ted Cook <teodoro.cook@gmail.com>
2021-11-12 19:14:38 -0600

    Set docker and gui install as optional

--- install.sh
+++ install.sh
@@ -7,16 +7,51 @@ do
     bash "$i"
 done
 
-sudo apt install -y autorandr
+OK=0
+KO=1
+
+GUI=$KO
+DOCKER=$KO
+
+# detect argument name
+ARGS=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --gui)
+        GUI=$OK
+        ARGS+=("$1")
+        shift
+        ;;
+        --docker)
+        DOCKER=$OK
+        ARGS+=("$1")
+        shift
+        ;;
+        *)
+        ARGS+=("\$1")
+        shift # past argument
+        ;;
+    esac
+done
+set -- "\${ARGS[@]}"
+
+curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-bash/master/install.sh | bash
+curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-emacs/master/install.sh | bash
+curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-tmux/master/install.sh | bash
+curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-vim/master/install.sh | bash
 
 export PYENV_VERSION=ansible
 
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-bash/master/install.sh | bash
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-tmux/master/install.sh | bash
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-i3/master/install.sh | bash
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-vim/master/install.sh | bash
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-emacs/master/install.sh | bash
-curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-docker/master/install.sh | bash
+if [[ $GUI == "$OK" ]]; then
+    sudo apt install -y autorandr
+    curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-i3/master/install.sh | bash
+fi
+
+if [[ $DOCKER == "$OK" ]]; then
+    curl -s https://raw.githubusercontent.com/nephelaiio/ansible-role-docker/master/install.sh | bash
+fi
+
+unset PYENV_VERSION
 
 sudo apt-get autoremove -y
-unset PYENV_VERSION
